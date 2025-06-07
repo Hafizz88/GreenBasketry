26f723720f0344ad08af93b765d10050b8df4eac
labib116 <2205116@ugrad.cse.buet.ac.bd>
2025-06-08 02:15:02 +0600

    yes now you can signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './components/login.js';
+import Signup from './components/Signup.js';
 import './App.css';
 
 
