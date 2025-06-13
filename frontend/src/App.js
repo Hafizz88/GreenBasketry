bdd4c8f811e01c29e190306589acf6e8df99b7f0
Nafin <rahmanmdhafi@gmail.com>
2025-06-14 02:07:47 +0600

    Resolved merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/login.js';
+import Login from './components/Login.js';
 import Signup from './components/Signup.js';
 import './App.css';
 
